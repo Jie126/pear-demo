e65aa2e6e75fa637760297ff37e5ce2c66189bf3
Jie126 <2518030927@qq.com>
2023-04-20 12:10:18 +0800

    避免重复请求

--- src/shared/Form.tsx
+++ src/shared/Form.tsx
@@ -40,7 +40,8 @@ export const FormItem = defineComponent({
     countFrom: {
       type: Number,
       default: 60
-    }
+    },
+    disabled: Boolean,
   },
   emits: ['update:modelValue'],
   setup: (props, context) => {
@@ -75,7 +76,7 @@ export const FormItem = defineComponent({
           return <>
             <input class={[s.formItem, s.input, s.validationCodeInput]}
               placeholder={props.placeholder} />
-            <Button disabled={isCounting.value} onClick={props.onClick} class={[s.formItem, s.button, s.validationCodeButton]}>
+            <Button disabled={isCounting.value || props.disabled} onClick={props.onClick} class={[s.formItem, s.button, s.validationCodeButton]}>
               {isCounting.value ? `${count.value}秒后可重新发送` : '发送验证码'}
             </Button>
           </>
